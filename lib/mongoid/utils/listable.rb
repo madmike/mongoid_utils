e3de3daa6d35ae56bc36b4bc2e65d295a16a972b
Michail <xbiznet@gmail.com>
2017-05-25 00:19:32 +0300

    fixed error with lists

--- lib/mongoid/utils/listable.rb
+++ lib/mongoid/utils/listable.rb
@@ -14,7 +14,8 @@ module Mongoid
     #    @hash ||= Hash[lists.map { |list| [list.model_name.param_key.to_sym, list] }]#.group_by(&:first).map { |k,v| {k => (el = v.map(&:last)).length > 1 ? el : el[0]} }.last || {}
         unless @hash
           hashes = lists.map{ |list| Hash[*[list.model_name.param_key.to_sym, list]] }
-          @hash ||= hashes.inject{ |h1,h2| h1.merge(h2){ |*a| a[1,2] } } || {}
+          @hash ||= hashes.flat_map(&:entries).group_by(&:first).map{|k,v| Hash[k, v.map(&:last)]}
+                    #hashes.inject{ |h1,h2| h1.merge(h2){ |*a| a[1,2] } } || {}
         end
 
         @hash
